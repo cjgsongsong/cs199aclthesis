dfa5ca457201da3d68b8bcba7fbc1d26ad2fbee6
CJ Songsong <cjgsongsong@gmail.com>
2022-05-28 21:16:42 +0800

    refactor

--- utils.py
+++ utils.py
@@ -1,6 +1,36 @@
 import fileinput
 from classes import Poset, LinearOrder
 
+def isAllConnected(vertices, relations):
+    for vertex in vertices:
+        isConnected = False
+        for relation in relations:
+            if vertex in relation:
+                isConnected = True
+                break
+        
+        if not isConnected: return False
+    
+    return True
+
+def generateRootedRelations(parent, vertices, relations):
+    if len(vertices) == 0:
+        return [sorted(relations)]
+    else:
+        rels = []
+        for child in vertices:
+            newVertices = [v for v in vertices if v != child]
+            rels.extend(generateRootedRelations(parent, newVertices, relations + [(parent, child)]))
+            rels.extend(generateRootedRelations(parent, newVertices, relations + [(child, parent)]))
+            rels.extend(generateRootedRelations(child, newVertices, relations + [(parent, child)]))
+            rels.extend(generateRootedRelations(child, newVertices, relations + [(child, parent)]))
+        
+        truerels = []
+        for rel in rels:
+            if rel not in truerels: truerels.append(rel)
+
+        return truerels
+
 def read(filepath):
     fileInput = open(filepath, "r")
     inputs = fileInput.readlines()
@@ -45,6 +75,36 @@ def split(txt):
     
     return data
 
+def countVertices(lo_input):
+    return lo_input.split('-')[0].count(',') + 1
+
+def countInversions(lo_input):
+    count = 0
+    los = [lo.sequence for lo in preprocess(lo_input)]
+    for i in range(len(los)):
+        lo_ref = los[i]
+        for j in range(i + 1, len(los)):
+            lo = [0 for k in range(len(lo_ref))]
+            for l in range(len(lo_ref)):
+                lo[los[j].index(lo_ref[l])] = l
+            
+            count += sum(pi > lo[j]
+                         for i, pi in enumerate(lo)
+                         for j in range(i + 1, len(lo)))
+    
+    return count
+
+def countSwapPairs(lo_input):
+    count = 0
+    los = [lo.relations for lo in preprocess(lo_input)]
+    for i in range(len(los)):
+        lo_ref = sorted(los[i])
+        for j in range(i + 1, len(los)):
+            lo = sorted([(b, a) for a, b in los[j]])
+            count += len(set(lo_ref).intersection(set(lo)))
+    
+    return count
+
 def _get_value(entry):
     return entry.strip("\n").split(":")[1][1:]
 
@@ -87,33 +147,3 @@ def verify(input, output, shouldLog = False):
         print(ell)
 
     return upsilon == ell
-
-def isAllConnected(vertices, relations):
-    for vertex in vertices:
-        isConnected = False
-        for relation in relations:
-            if vertex in relation:
-                isConnected = True
-                break
-        
-        if not isConnected: return False
-    
-    return True
-
-def generateRootedRelations(parent, vertices, relations):
-    if len(vertices) == 0:
-        return [sorted(relations)]
-    else:
-        rels = []
-        for child in vertices:
-            newVertices = [v for v in vertices if v != child]
-            rels.extend(generateRootedRelations(parent, newVertices, relations + [(parent, child)]))
-            rels.extend(generateRootedRelations(parent, newVertices, relations + [(child, parent)]))
-            rels.extend(generateRootedRelations(child, newVertices, relations + [(parent, child)]))
-            rels.extend(generateRootedRelations(child, newVertices, relations + [(child, parent)]))
-        
-        truerels = []
-        for rel in rels:
-            if rel not in truerels: truerels.append(rel)
-
-        return truerels
